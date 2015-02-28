b0035fcdef6d98ac5136968a7180808f9080c836
Harvey <ch4@users.noreply.github.com>
2015-02-27 23:51:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var guides = db.collection('guide');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var server = app.listen(3000, function () {
 
