599d3fa98c3623296651a7a493059eb5b7b10cf5
David R <dpricha189@gmail.com>
2015-02-27 23:34:07 -0800

    For Harvey

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser.json())
 var moment = require('moment');
 var fs = require('fs');
-
+var path = require('path');
 
 
 //Tables
@@ -12,6 +11,9 @@ var mongojs = require('mongojs');
 var db = mongojs('admin:batsignal@ds041861.mongolab.com:41861/batsignal');
 var userscollection = db.collection('users');
 var guides = db.collection('guide');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 
 var server = app.listen(3000, function () {
 
@@ -21,9 +23,7 @@ var server = app.listen(3000, function () {
         console.log('Example app listening at http://%s:%s', host, port);
 
 });
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(bodyParser.json());
+
 
 
 
@@ -36,11 +36,13 @@ app.get('/createevent', function(req, res){
 });
 
 app.post('/createevent', function(req, res){
+  console.log(req.body);
 	if(!req.body.title || !req.body.category || !req.body.timeStart || !req.body.timeEnd || !req.body.price || !req.body.description){
      return res.send(400, 'Missing guide ID');
+
 	}
     else{
-	guides.update({'title': req.params.title}, {"title": req.body.title, "category", req.body.category,"timeStart":req.body.timeStart,"timeEnd": req.body.timeEnd,"price": req.body.price, "description": req.body.description, "eventPhoto": 'photos/' + req.body.title}, {upsert: true});
+	guides.update({'title': req.body.title}, {"title": req.body.title, "category": req.body.category,"timeStart":req.body.timeStart,"timeEnd": req.body.timeEnd,"price": req.body.price, "description": req.body.description, "eventPhoto": 'photos/' + req.body.title}, {upsert: true});
 			if(req.body.photo){
 				var random = Math.floor((Math.random() * 100) + 1);
 		        var newPath = __dirname + 'photos/' + req.body.title + "/" +  random + ".jpg";
