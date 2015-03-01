ff0ebc3c86a88cb95ccad41d045f63b7f15f8a73
David R <dpricha189@gmail.com>
2015-03-01 10:06:26 -0800

    Made changes to home

--- controllers/home.js
+++ controllers/home.js
@@ -3,16 +3,30 @@
  * Home page.
  */
 exports.index = function(req, res) {
+  var months = [
+      "January",  
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"  
+      ]
 	console.log(req.user)
   if(typeof req.user == 'undefined'){	
   	console.log("profile not available")
   res.render('../public/index.ejs', {
-    title: 'Home', user: false
+    title: 'Home', user: false, "monthdrop": months
    });
    }else{
    	console.log("profile available")
     res.render('../public/index.ejs', {
-    title: 'Home', "user": req.user
+    title: 'Home', "user": req.user, "monthdrop": months
   });
 }
 };
